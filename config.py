a93edf2f0a6c6cb8e41ddc8d818362e2ca98d466
小神1024 <48399687+Sjj1024@users.noreply.github.com>
2021-12-19 18:23:14 +0800

    保存session

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+from redis import StrictRedis
+
+
 class Config(object):
     """
     项目配置
@@ -7,3 +10,12 @@ class Config(object):
     # 数据库配置
     SQLALCHEMY_DATABASE_URI = "mysql://root:123456@localhost:3306/infomation"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
+
+    SESSION_TYPE = "redis"
+    SESSION_USE_SIGNER = True
+    PERMANENT_SESSION_LIFETIME = 86400 * 2
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    SECRET_KEY = "daasdasetasrysrywerywersdadfasdfasasfasdfasdfasdfasfds"
--- run.py
+++ run.py
@@ -1,7 +1,10 @@
-from flask import Flask
-from config import Config
+from flask import Flask, session
+from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
+from redis import StrictRedis
 
+from config import Config
 
 app = Flask(__name__)
 
@@ -10,10 +13,14 @@ app.config.from_object(Config)
 # 初始化数据库
 db = SQLAlchemy()
 db.init_app(app)
-
+# 设置session保存位置
+redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+Session(app)
+CSRFProtect(app)
 
 @app.route("/")
 def index():
+    session["name"] = "song"
     return "首页"
 
 
