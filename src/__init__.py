b9e56b49d6c7f72fa653c3667cd9c473e31324e1
songjj <songjj@zetyun.com>
2022-12-28 14:34:13 +0800

    init

--- src/__init__.py
+++ src/__init__.py
@@ -2,23 +2,10 @@ import logging
 import os
 import pkgutil
 import sys
-from logging.handlers import RotatingFileHandler, TimedRotatingFileHandler
-
+from logging.handlers import TimedRotatingFileHandler
 from flask import Flask, Blueprint
 from flask_session import Session
-from flask_sqlalchemy import SQLAlchemy
-from flask_wtf import CSRFProtect
-from redis import StrictRedis
-
 from config import config
-
-# 给变量加注释，让其可以自动提示
-redis_store = None  # type: StrictRedis
-db: SQLAlchemy = None
-
-
-# 下面这种也是一样的，变量提示，后面引入之后也可以自动获取提示
-# redis_store: StrictRedis = None
 
 
 def setup_log(log_level):
@@ -70,15 +57,8 @@ def creat_app(con: str):
     if config_env is not None:
         con = config_env
     app.config.from_object(config[con])
-    # 初始化数据库
-    global db
-    db = SQLAlchemy()
-    db.init_app(app)
     # 日志等级配置
     setup_log(config[con].LOG_LEVEL)
-    # 设置session保存位置: 配置对象里面的属性是类属性
-    global redis_store
-    redis_store = StrictRedis(host=config[con].REDIS_HOST, port=config[con].REDIS_PORT)
     # 可以指定session的保存位置，要在app的config中配置
     Session(app)
     # 开启CSRF保护
--- src/moduls/index/views.py
+++ src/moduls/index/views.py
@@ -1,38 +1,23 @@
 import logging
 from flask import jsonify
 from . import index_blu
-from ... import redis_store, db
-from ...models import User
 
 
 @index_blu.route("/")
 def index():
     logging.info("访问首页")
     # 使用session存储session
-    print(redis_store)
-    redis_store.set("name", "song")
-    res = db.session.execute("select nick_name from users").fetchall()
-    emp_json_list = [dict(zip(item.keys(), item)) for item in res]
-    print(emp_json_list)
     return "首页内容"
 
 
 @index_blu.route("/user")
 def query_user():
     # 返回所有用户保存到list中
-    user_list = User.query.all()
-    print(user_list)
-    result = [u.to_json() for u in user_list]
-    return jsonify(result), 200
+    user_list = ["1"]
+    return jsonify(user_list), 200
 
 
 @index_blu.route("/adduser")
 def add_user():
     # 测试数据库
-    u = User()
-    u.nick_name = "wang"
-    # 将用户添加到数据库会话中
-    db.session.add(u)
-    # 将数据库会话中的变动提交到数据库中,如果不Commit,数据库中是没有改动的
-    db.session.commit()
     return "添加成功"
