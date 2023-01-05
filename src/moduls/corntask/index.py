f61933d1db588831eb7d8d811326726173d218f1
songjj <songjj@zetyun.com>
2023-01-05 20:59:13 +0800

    add:corn task

--- src/moduls/corntask/index.py
+++ src/moduls/corntask/index.py
@@ -9,11 +9,11 @@ dir_path = r'E:\桌面\民宿'
 sched = BackgroundScheduler()
 
 # 开始添加定时任务
-sched.add_job(times, CronTrigger.from_crontab('*/5 * * * *'))
+# sched.add_job(times, CronTrigger.from_crontab('*/5 * * * *'))
 # args用于函数中进行传值,可以随机命名,只需要在后面传递参数即可
-sched.add_job(delFiles, CronTrigger.from_crontab('*/5 * * * *'), args=(dir_path, "30"))
-# 定时更新caoliu用户信息
-# sched.add_job(update_caoliu_info, CronTrigger.from_crontab('*/1 * * * *'))
+# sched.add_job(delFiles, CronTrigger.from_crontab('*/5 * * * *'), args=(dir_path, "30"))
+# 定时更新caoliu用户信息: 每12小时
+sched.add_job(update_caoliu_info, CronTrigger.from_crontab('0 */12 * * *'))
 # 因为定时任务sched.start是阻塞的，所以可以放到一个线程里面执行
 t = Thread(target=sched.start)
 t.start()
