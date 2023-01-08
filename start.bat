0d9da3ac549786559185b9d610500d1d08801aea
songjj <songjj@zetyun.com>
2023-01-08 18:58:32 +0800

    add:some

--- start.bat
+++ start.bat
@@ -1,3 +1,15 @@
-git pull
-pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple/
-python app.py
+@echo off
+:start
+    git pull > tmp.txt
+    set tmp_var=<tmp.txt
+    del tmp.txt
+    echo %tmp_var%| findstr out >nul && (
+        echo 'time out reGet...'
+    ) || (
+        echo 'success, start run App...'
+        goto :runApp
+    )
+:runApp
+    pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple/
+    python app.py
+for /l %a in (0,0,1) do goto :start
