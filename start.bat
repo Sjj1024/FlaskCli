30a3b03e04df89c36a1d4ee96f21649f13faa117
songjj <songjj@zetyun.com>
2023-01-08 19:22:13 +0800

    add:some

--- start.bat
+++ start.bat
@@ -1,14 +1,7 @@
-:start
-    git pull > tmp.txt
-    set tmp_var = <tmp.txt
-    set b = fatal
-    echo %tmp_var%| findstr %b% >nul && (
-        echo time out re run get pull...
-    ) || (
-        echo success, start run App...
-        goto :runApp
-    )
-:runApp
-    pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple/
-    python app.py
-for /l %a in (0,0,1) do goto :start
+git pull
+git pull
+git pull
+git pull
+git pull
+pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple/
+python app.py
