206b21110353e74001e2d25f0fb4ea07b925bc4c
songjj <songjj@zetyun.com>
2023-04-05 20:07:01 +0800

    add:some

--- src/moduls/corntask/task_views.py
+++ src/moduls/corntask/task_views.py
@@ -42,8 +42,8 @@ def add_caoliu_commit_local():
             update_user = update_user_list.all()[0].to_json()
         else:
             return jsonify(code=207, message="没有查找到该用户")
-        if update_user["task_file_sha"]:
-            return jsonify(code=205, message="error", data=f"任务已存在")
+        if user_name in update_user["task_file_sha"]:
+            return jsonify(code=205, message="任务已存在", data=f"任务已存在")
         task_id = add_caoliu_commit_task(user_name, cookie, user_agent, corn_tab)
         update_user["task_file_sha"] = task_id
         update_user["task_status"] = "已开启"
